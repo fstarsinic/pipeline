36341acad168f4f28a9ce2e628f2f5a5569a4c8a
Frank Starsinic <frank.starsinic@gmail.com>
2022-06-29 14:41:34 -0700

    fixed stuff

--- Stage.py
+++ Stage.py
@@ -1,6 +1,6 @@
-import inspect
+""" Is this the right spot for module docstring?"""
 import logging
-
+import sys
 
 class Stage:
     """ This is the base class for a pipeline Stage"""
@@ -12,10 +12,13 @@ class Stage:
 
     def __init__(*args, **kwargs):
         """ constructor """
-        logging.info('__init__()')
+        logging.info(f'__init__({args}, {kwargs}')
+        first = args[0]
+        print(f'{result}')
 
     def initalize(self, *args, **kwargs):
         """ initialize method """
+
 
     def execute(self, *args, **kwargs):
         """ execute method """
@@ -26,9 +29,11 @@ class Stage:
     def update_state(self):
         """ updates the state for the given stage"""
 
-def main():
+def main(stuff):
     """ test method """
     print('Hello')
+    i = int(stuff)
+    s = Stage(i)
 
 if __name__ == '__main__':
-    main()
+    main(4)
